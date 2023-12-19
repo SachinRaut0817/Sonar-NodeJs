0b3158fe7dbe2e8099892167abeebb9958f4894f
SachinRaut0817 <117826372+SachinRaut0817@users.noreply.github.com>
2023-12-19 16:17:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, Sachin");
+// Simple index.js file
+
+// Function to add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Example usage
+const result = addNumbers(5, 3);
+console.log(`The result is: ${result}`);
